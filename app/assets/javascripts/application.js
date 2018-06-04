29f7c3e66125d9d785a2260a5d67193540ac6a39
Christopher Campbell <christopher.campbell@outlook.com>
2018-06-04 19:30:16 -0400

    Fix error in top bar default drop down, remove the 3 from require juery3 in js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+//= require_tree
 //= require_tree .
 
-//= require foundation
+
 $(function(){ $(document).foundation(); });
