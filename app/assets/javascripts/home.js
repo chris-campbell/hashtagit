c86534f3c8dab8ddb305f808aa8a3e4d30b15b9b
Christopher Campbell <christopher.campbell@outlook.com>
2018-06-09 12:53:42 -0400

    Error: No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://localhost:3000' is therefore not allowed access

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,6 +3,6 @@ $('.btn').click(function() {
   $.get("https://api.ritekit.com/v1/stats/multiple-hashtags?tags=" + result +
             "&client_id=71877ac7a47d252480110b17cb8cdfc5421e1c47dc8b",
             function( data ) {
-              console.log( data );
+              console.log( data);
             });
 });
