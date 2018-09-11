36ca19bd90f202848d187eea4609ebaf192641c9
Kris Camp <kris.camp@outlook.com>
2018-09-11 01:10:18 +0000

    Remove Rubocop violations from test files

--- spec/controllers/queries_controller_spec.rb
+++ spec/controllers/queries_controller_spec.rb
@@ -1,46 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe QueriesController, type: :controller do
-    
-    describe 'queries#index' do 
-        it "Should successfully show page" do
-            get :index
-            expect(response).to have_http_status(302)
-        end
+
+ describe 'queries#index' do
+    it 'should successfully show page' do
+      get :index
+      expect(response).to have_http_status(302)
     end
-    
-    describe 'queries#apiquery' do
-        it "Should successfully spam" do
-            get :apiquery
-            expect(response).to have_http_status(302)
-        end
+  end
+
+  describe 'queries#apiquery' do
+    it 'should successfully spam' do
+      get :apiquery
+      expect(response).to have_http_status(302)
     end
-    
-    describe 'queries#create' do
-        it "Should require user to be signed in" do
-            post :create, params: { query: { tweet_sum: 444, retweet_sum: 0331,
-            exposure_sum: 0610, hashtag: 'blacklove' } }
-            expect(response).to redirect_to new_user_session_path
-        end
-        
-        it "Should create a query in the database" do
-            user = FactoryBot.create(:user)
-            sign_in user
-            
-            post :create, params: {
-                query: { 
-                    tweet_sum: 444, 
-                    retweet_sum: 0331,
-                    exposure_sum: 0610,
-                    hashtag: 'blacklove'
-                }
-            }
-            
-            expect(response).to redirect_to root_path
-            query = Query.last
-            expect(query.hashtag).to eq('blacklove')
-            expect(Query.count).to eq 1
-        end
+  end
+
+  describe 'queries#create' do
+    it 'should require user to be signed in' do
+      post :create, params: { query: { tweet_sum: 444, retweet_sum: 0331,
+                              exposure_sum: 0610, hashtag: 'blacklove' } }
+      expect(response).to redirect_to new_user_session_path
     end
-    
+
+    it 'should create a query in the database' do
+      user = FactoryBot.create(:user)
+      sign_in user
+
+      post :create, params: {
+        query: {
+          tweet_sum: 444,
+          retweet_sum: 0331,
+          exposure_sum: 0610,
+          hashtag: 'blacklove'
+        }
+      }
+
+      expect(response).to redirect_to root_path
+      query = Query.last
+      expect(query.hashtag).to eq('blacklove')
+      expect(Query.count).to eq 1
+    end
+  end
 end
--- spec/models/query_spec.rb
+++ spec/models/query_spec.rb
@@ -1,43 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Query, type: :model do
-    describe "format_results" do
-        
-        it "should should 'N/A' if value is equal to 0" do
-            query = FactoryBot.create(:query)
-            
-            value = query.tweet_sum
-       
-            expect(query.format_results(value)).to eq 'N/A'
-        end
-        
-        it "should return value if value is less 1000 & greater than 0" do
-            query = FactoryBot.create(:query, tweet_sum: 435)
-         
-            value = query.tweet_sum
-            expect(query.format_results(value)).to eq '435'
-        end
-        
-        it "should return value with appended 'K' if greater than 1000 and less than 1000000" do
-            query = FactoryBot.create(:query, tweet_sum: 1001)
-            
-            value = query.tweet_sum
-            expect(query.format_results(value)).to eq '1.0K'
-        end
-        
-        it "should return value with appened 'M' if greater 1000000 and less than 1000000000" do
-            query = FactoryBot.create(:query, tweet_sum: 1000001)
-            
-            value = query.tweet_sum
-            expect(query.format_results(value)).to  eq '1.0M'
-        end
-        
-        it "should return value with appended 'B' if greater 100000000 and less than 100000000000" do
-            query = FactoryBot.create(:query, tweet_sum: 1000001)
-            
-            value = query.tweet_sum
-            expect(query.format_results(value)).to eq '1.0B'
-        end
+  describe 'format_results' do
+
+    it 'should should N/A if value is equal to 0' do
+      query = FactoryBot.create(:query)
+      value = query.tweet_sum
+      expect(query.format_results(value)).to eq 'N/A'
     end
+
+    it 'should return value if value is less 1000 & greater than 0' do
+      query = FactoryBot.create(:query, tweet_sum: 435)
+      value = query.tweet_sum
+      expect(query.format_results(value)).to eq '435'
+    end
+
+    it 'appended K if greater than 1000 and less than 1000000' do
+      query = FactoryBot.create(:query, tweet_sum: 1001)
+      value = query.tweet_sum
+      expect(query.format_results(value)).to eq '1.0K'
+    end
+
+    it 'appened M if greater 1000000 and less than 1000000000' do
+      query = FactoryBot.create(:query, tweet_sum: 1000001)
+      value = query.tweet_sum
+      expect(query.format_results(value)).to eq '1.0M'
+    end
+
+    it 'appended B if greater 100000000 and less than 100000000000' do
+      query = FactoryBot.create(:query, tweet_sum: 1000001)
+      value = query.tweet_sum
+      expect(query.format_results(value)).to eq '1.0B'
+    end
+  end
 end
-        
